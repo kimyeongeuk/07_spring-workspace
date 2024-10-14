d564a88d7b649c3eb2262b9d0a3c7efe1916c9ac
kimyeongeuk <tklr0731@naver.com>
2024-10-14 17:05:09 +0900

    Initial commit

---
+++ 01_Spring_IoC_DI/src/main/java/com/br/spring/ioc/xml/Calculator.java
@@ -0,0 +1,14 @@
+package com.br.spring.ioc.xml;
+
+public class Calculator {
+	
+	public void plus(int x,int y) {
+		System.out.printf("%d + %d = %d\n",x,y,x+y);
+	}
+	
+	public void minus(int x,int y) {
+		System.out.printf("%d - %d = %d\n",x,y,x-y);
+	}
+	
+
+}
---
+++ 01_Spring_IoC_DI/src/main/java/com/br/spring/ioc/xml/Student.java
@@ -0,0 +1,71 @@
+package com.br.spring.ioc.xml;
+
+public class Student {
+	
+	private String name;
+	private String academy;
+	private String classColor;
+	private Calculator calc;
+	
+	
+	public Student() {}
+
+
+	public Student(String name, String academy, String classColor, Calculator calc) {
+		super();
+		this.name = name;
+		this.academy = academy;
+		this.classColor = classColor;
+		this.calc = calc;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getAcademy() {
+		return academy;
+	}
+
+
+	public void setAcademy(String academy) {
+		this.academy = academy;
+	}
+
+
+	public String getClassColor() {
+		return classColor;
+	}
+
+
+	public void setClassColor(String classColor) {
+		this.classColor = classColor;
+	}
+
+
+	public Calculator getCalc() {
+		return calc;
+	}
+
+
+	public void setCalc(Calculator calc) {
+		this.calc = calc;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", academy=" + academy + ", classColor=" + classColor + ", calc=" + calc + "]";
+	}
+	
+	
+	
+
+}
