723259de131f93a66b6d85513f20c66502fc21a2
kimyeongeuk <tklr0731@naver.com>
2024-10-25 17:47:44 +0900

    spring

--- springWeb/src/test/java/com/br/spring/dao/MemberDaoUnitTest_JUnit4.java
+++ springWeb/src/test/java/com/br/spring/dao/MemberDaoUnitTest_JUnit4.java
@@ -20,6 +20,42 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class) // JUnit4를 이용하겠다는걸 의미
 public class MemberDaoUnitTest_JUnit4 {
 
+	/*
+	 * 테스트 케이스 작성 패턴
+	 * 
+	 * given (준비) : 어떤 데이터를 가지고
+	 * when	 (실행) : 어떤 메소드 실행시
+	 * then  (검증) : 어떤 결과가 나와야되는지
+	 * 			ㄴ assertEquals(예상값, 실행값) : 실행값이 예상값과 일치하는지 확인해주는 메소드
+	 * 			ㄴ assertNotNull(실행값)		: 실행값이 null이 아닌지 확인해주는 메소드
+	 * 			ㄴ assertTrue(조건)				: 해당 조건이 참인지를 확인해주는 메소드 
+	 */
 	
+	@Test
+	public void test01_로그인테스트() {
+		
+	}
 
+	@Test
+	public void test02_회원가입테스트() {
+		
+	}
+	
+	@Test
+	public void test03_일치하는아이디수조회테스트() {
+		
+	}
+	
+	@Test
+	public void test04_회원정보변경테스트() {
+		
+	}
+	
+	@Test
+	public void test05_회원탈퇴테스트() {
+		
+	}
+	
+	
+	
 }
